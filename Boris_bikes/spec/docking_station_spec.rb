3f34df3e557afe2bdfbf514d864ce0079bea1661
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-09 14:30:13 +0000

    Amended test to use one-line format

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -1,8 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "release bike" do
-    docking_station = DockingStation.new
-  expect(docking_station).to respond_to('release_bike')
-end
+#   it "release bike" do
+#     # docking_station = DockingStation.new
+#   expect(docking_station).to respond_to('release_bike')
+# end
+it { is_expected.to respond_to('release_bike') }
+# it {should respond_to 'release_bike'} - another example
+
 end
