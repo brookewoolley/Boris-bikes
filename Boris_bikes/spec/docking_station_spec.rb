6d268218413bc06c0949ee733349eccb36433d82
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-09 14:19:04 +0000

    Wrote test for docking station to release bike

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-  
+  it "release bike" do
+    docking_station = DockingStation.new
+  expect(docking_station).to respond_to('release_bike')
+end
 end
