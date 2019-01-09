f4b767a6579249f5547bb5c025c38a0a45e58b56
Erin Stevenson <estevenson_94@live.com>
2019-01-09 14:42:50 +0000

    Adding bike_spec.rb

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
 #     # docking_station = DockingStation.new
 #   expect(docking_station).to respond_to('release_bike')
 # end
-it { is_expected.to respond_to('release_bike') }
+it { is_expected.to respond_to :release_bike }
 # it {should respond_to 'release_bike'} - another example
 
 end
