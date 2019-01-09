e229c97e7ec41086af0c2f6820e9efa2ebe8dc56
Erin Stevenson <estevenson_94@live.com>
2019-01-09 15:05:49 +0000

    Test for release_bike to return a Bike instance

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -2,10 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
 #   it "release bike" do
-#     # docking_station = DockingStation.new
+     docking_station = DockingStation.new
 #   expect(docking_station).to respond_to('release_bike')
 # end
 it { is_expected.to respond_to :release_bike }
 # it {should respond_to 'release_bike'} - another example
+
+   it 'gets a bike' do
+    expect(docking_station.release_bike).to be_a Bike 
+    end
 
 end
